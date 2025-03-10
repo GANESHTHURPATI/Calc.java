f23bc788420803727ce975ce0bc657e526068a07
GANESHTHURPATI <ganeshthurpati0507@gmail.com>
2025-03-10 13:19:55 +0530

    Create Grade.java

---
+++ Grade.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class GradeCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Asking for the number of subjects
+        System.out.print("Enter the number of subjects: ");
+        int numSubjects = scanner.nextInt();
+
+        // Array to store marks
+        int[] marks = new int[numSubjects];
+        int totalMarks = 0;
+
+        // Taking input for each subject
+        for (int i = 0; i < numSubjects; i++) {
+            System.out.print("Enter marks for subject " + (i + 1) + ": ");
+            marks[i] = scanner.nextInt();
+            totalMarks += marks[i];
+        }
+
+        // Calculating percentage
+        double percentage = (double) totalMarks / (numSubjects * 100) * 100;
+
+        // Assigning grade
+        char grade;
+        if (percentage >= 90) {
+            grade = 'A';
+        } else if (percentage >= 80) {
+            grade = 'B';
+        } else if (percentage >= 70) {
+            grade = 'C';
+        } else if (percentage >= 60) {
+            grade = 'D';
+        } else if (percentage >= 50) {
+            grade = 'E';
+        } else {
+            grade = 'F';
+        }
+
+        // Displaying results
+        System.out.println("\nTotal Marks: " + totalMarks);
+        System.out.println("Percentage: " + percentage + "%");
+        System.out.println("Grade: " + grade);
+
+        // Closing scanner
+        scanner.close();
+    }
+}
